f04cab24cc3d29dd2bba395ff729dd5730c74381
Terentev <terentev.space@gmail.com>
2020-03-28 18:19:56 +0700

    e

---
+++ ConsoleApp5/Instrument.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Instrument
+    {
+        [JsonProperty(PropertyName = "symbol")]
+        public string Symbol { get; set; }
+
+        [JsonProperty(PropertyName = "fairPrice")]
+        public double Price { get; set; }
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = client.GetAsync("https://www.bitmex.com/api/v1/instrument/active").Result;
+                string result = response.Content.ReadAsStringAsync().Result;
+
+                List<Instrument> list = JsonConvert.DeserializeObject<List<Instrument>>(result);
+
+                Console.Clear();
+                foreach (Instrument item in list)
+                {
+                    Console.WriteLine("Name: " + item.Symbol);
+                    Console.WriteLine("Price: " + item.Price);
+                    Console.WriteLine("################################");
+                }
+                Thread.Sleep(10000);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
